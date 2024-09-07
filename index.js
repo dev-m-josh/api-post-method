6562a4fa62fbb1ed81168677b8f11137c42c0f9b
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-09-07 13:08:45 +0300

    spellings edit

--- index.js
+++ index.js
@@ -40,7 +40,32 @@ app.get('/fruits', (req,res)=>{
 app .post('/fruits', (req, res)=>{
     console.log(req.body);
     fruits.push(req.body);
-    res.status(201).send("Fruits added succesifully!");
+    res.status(201).send("Fruits added successfully!");
 });
+
+const cars = [
+    { id: 1, make: 'Toyota', model: 'Camry', year: 2020 },
+    { id: 2, make: 'Honda', model: 'Civic', year: 2019 },
+    { id: 3, make: 'Ford', model: 'Mustang', year: 2021 },
+    { id: 4, make: 'Chevrolet', model: 'Malibu', year: 2018 },
+    { id: 5, make: 'Tesla', model: 'Model S', year: 2022 },
+    { id: 6, make: 'Nissan', model: 'Altima', year: 2020 },
+    { id: 7, make: 'BMW', model: '3 Series', year: 1960 },
+    { id: 8, make: 'Audi', model: 'A4', year: 2020 },
+    { id: 9, make: 'Mercedes-Benz', model: 'C-Class', year: 2019 },
+    { id: 10, make: 'Lexus', model: 'RX', year: 2021 }
+];
+
+//get cars list
+app.get('/cars', (req, res)=>{
+    res.json(cars)
+})
+
+//post a new car
+app.post('/cars', (req, res)=>{
+    console.log(req.body)
+    cars.push(req.body);
+    res.status(201).send("New car added successfully!")
+})
 
 app.listen(400, ()=>{console.log("Server started running on port:400")});
