9579648307728366b2cfc2a3ead4bcc7d2d10307
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-09-08 20:57:59 +0300

    code edit 'remove cars array'

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+//to use in the postman JSON
 app.use(express.json());
+
+//to use in the postman urlencoded
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res)=>{
@@ -43,29 +46,6 @@ app .post('/fruits', (req, res)=>{
     res.status(201).send("Fruits added successfully!");
 });
 
-const cars = [
-    { id: 1, make: 'Toyota', model: 'Camry', year: 2020 },
-    { id: 2, make: 'Honda', model: 'Civic', year: 2019 },
-    { id: 3, make: 'Ford', model: 'Mustang', year: 2021 },
-    { id: 4, make: 'Chevrolet', model: 'Malibu', year: 2018 },
-    { id: 5, make: 'Tesla', model: 'Model S', year: 2022 },
-    { id: 6, make: 'Nissan', model: 'Altima', year: 2020 },
-    { id: 7, make: 'BMW', model: '3 Series', year: 1960 },
-    { id: 8, make: 'Audi', model: 'A4', year: 2020 },
-    { id: 9, make: 'Mercedes-Benz', model: 'C-Class', year: 2019 },
-    { id: 10, make: 'Lexus', model: 'RX', year: 2021 }
-];
 
-//get cars list
-app.get('/cars', (req, res)=>{
-    res.json(cars)
-})
-
-//post a new car
-app.post('/cars', (req, res)=>{
-    console.log(req.body)
-    cars.push(req.body);
-    res.status(201).send("New car added successfully!")
-})
 
 app.listen(400, ()=>{console.log("Server started running on port:400")});
